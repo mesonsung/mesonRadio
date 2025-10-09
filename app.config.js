ed6cf4e1c09e7e532e6bc1637180503347a90664
Meson Sung <meson.sung@gmail.com>
2025-10-09 09:17:08 +0800

    v1.0.2

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'mesonRadio',
     slug: 'mesonradio',
-    version: '1.0.1',
+    version: '1.0.2',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'automatic',
